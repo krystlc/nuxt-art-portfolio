583f8a308a209cb7093db511eba49eff7dcfb822
Alex <alex.valle@gmail.com>
2020-07-05 12:14:35 -0400

    fixed contact form

---
+++ pages/confirmed.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="Confirmed" class="py-8 container text-center">
+    <h3 class="title">Confirmed!</h3>
+    <p>I'll be in touch shortly.</p>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Confirmed!',
+    }
+  },
+})
+</script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -7,29 +7,28 @@
       </div>
     </section>
     <section class="container">
-      <form
-        name="contact"
-        method="POST"
-        netlify-honeypot="bot-field"
-        netlify
-        data-netlify="true"
-        action="/"
-      >
-        <input type="text" name="bot-field" value />
+      <form name="contact" action="/confirmed" method="post" netlify-honeypot="bot-field" netlify>
+        <input type="hidden" name="form-name" value="contact" />
+        <p class="hidden">
+          <label>
+            Don’t fill this out:
+            <input name="bot-field" />
+          </label>
+        </p>
         <div class="flex flex-wrap -mx-3 md:mb-6">
           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label for="Name">Name</label>
-            <input id="Name" class="py-3 px-4" type="text" value />
+            <input id="Name" name="Name" class="py-3 px-4" type="text" value />
           </div>
           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label for="Email">Email</label>
-            <input id="Email" class="py-3 px-4" type="email" value />
+            <input id="Email" name="Email" class="py-3 px-4" type="email" value />
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 md:mb-6">
           <div class="w-full px-3 mb-6 md:mb-0">
             <label for="Message">Message</label>
-            <textarea id="Message" class="py-3 px-4" value />
+            <textarea id="Message" name="Message" class="py-3 px-4" value />
           </div>
         </div>
         <button class="btn btn-primary" type="submit">
