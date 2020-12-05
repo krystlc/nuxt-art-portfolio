45a57faffcbb83593d4986c1d7af5c9ef50b794a
Alex <alex.valle@gmail.com>
2020-12-05 12:16:58 -0500

    added body and links to landing page

--- pages/landing/_slug.vue
+++ pages/landing/_slug.vue
@@ -1,36 +1,24 @@
 <template>
-  <div id="LandingPage">
+  <div id="LandingPage" class="text-center">
     <section class="hero">
       <div class="container">
         <h3 class="title" v-text="title" />
         <p v-if="description" class="subtitle" v-text="description" />
       </div>
     </section>
-    <section class="links">
-      <div v-for="link in links" :key="link.url" class="link">
-        <a :href="link.url" target="_blank" v-text="link.label" />
+    <section v-if="body" class="container landing__body">
+      <div v-html="body" />
+    </section>
+    <section class="container links">
+      <div v-for="link in links" :key="link.url" class="links__link">
+        <a
+          :href="link.url"
+          target="_blank"
+          class="btn btn-secondary"
+          v-text="link.label"
+        />
       </div>
     </section>
-    <!-- <section class="container links">
-      <div class="collection__details">
-        <p>Released: {{ entry.fields.released | date }}</p>
-        <div v-html="body" />
-      </div>
-      <div class="collection__items">
-        <figure v-for="item in items" :id="item.id" :key="item.id" class="collection__item">
-          <picture>
-            <source :srcset="`${item.image}?fm=webp&w=960&q=80`" type="image/webp" />
-            <source :srcset="`${item.image}?fm=jpg&w=960&q=90`" type="image/jpeg" />
-            <img :src="`${item.image}?fm=jpg&w=960&q=90`" :alt="item.title" :title="item.title" />
-          </picture>
-          <figcaption>
-            <strong>{{ item.title }}.</strong>
-            <span>{{ item.tags }}.</span>
-            <span>{{ item.description }}</span>
-          </figcaption>
-        </figure>
-      </div>
-    </section> -->
   </div>
 </template>
 
@@ -78,30 +66,14 @@ export default EntryMixin.extend({
 </script>
 
 <style scoped>
-.collection {
-  @apply mt-8;
+.links,
+.landing__body {
+  @apply pt-8;
 }
-.collection figcaption {
-  @apply mt-4 italic;
+.links__link {
+  @apply py-4;
 }
-.collection__item {
-  @apply my-8;
-}
-.collection__item:last-child {
-  @apply mb-0;
-}
-.collection__details .tags {
-  @apply italic uppercase text-xs;
-}
-@screen sm {
-  .collection {
-    @apply grid grid-cols-3 gap-8;
-  }
-  .collection__items {
-    @apply col-span-2;
-  }
-  .collection__item {
-    @apply mt-0;
-  }
+.links__link .btn {
+  @apply inline-block;
 }
 </style>
